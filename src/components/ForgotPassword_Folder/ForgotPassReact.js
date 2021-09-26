892453e6c6284d877ecec2c7278913094642348e
Zubayr Khalid <54028598+ZubayrKal@users.noreply.github.com>
2021-09-26 15:34:24 +0200

    forgetpasswordedit

--- src/components/ForgotPassword_Folder/ForgotPassReact.js
+++ src/components/ForgotPassword_Folder/ForgotPassReact.js
@@ -6,9 +6,14 @@ export default function ForgotPassReact(props) {
 
     return (
         <div>
-           <div className="w-100 text-center mt-2">
-                    New User? <Link to="/signup">Sign Up</Link>
-            </div>
+           
+           <div  className="w-100  mt-5" style={{textAlign:'left', marginLeft:'50%'}}>
+                                <h6><strong>Already an User? </strong> 
+                                    <strong> Then </strong>
+                                    <Link to="/login" style={{ textDecoration: 'none', color: '#B429F9' }}><strong>login now!</strong></Link>
+                                </h6>
+                     </div>
+
         </div>
     )
 }
--- src/components/ForgotPassword_Folder/ForgotPassword.js
+++ src/components/ForgotPassword_Folder/ForgotPassword.js
@@ -1,8 +1,9 @@
 import React, {useRef, useState} from 'react'
-import {Card, Button, Form, Alert} from 'react-bootstrap'
+import { Button, Form, Alert} from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import { useAuth } from '../../context/AuthContext'
 import ForgotPassReact from './ForgotPassReact'
+import { Segment, } from 'semantic-ui-react'
 
 
 export default function ForgotPassword() {
@@ -40,34 +41,35 @@ export default function ForgotPassword() {
     }
 
     return (
-        <div>
-            <Card>
-                <Card.Body>
-                    <h2 className="text-center mb-4">Reset Password</h2>
+        <div style={{height: '80vh', marginLeft: '35%', marginRight: '35%'}} >
+            
+            <Segment vertical textAlign='center' style={{top:'25%'}} >
+           
+                    <h2 className="text-center mb-4" style={{ textDecoration: 'none', color: '#B429F9' }}>Reset Password</h2>
 
                     {error && <Alert variant="danger">{error}</Alert>}
                     {message && <Alert variant="success">{message}</Alert>}
 
                     <Form onSubmit={handleSubmit}>
                         <Form.Group id="email">
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" required ref={emailRef}></Form.Control>
+
+                            <Form.Control type="email"  placeholder="Enter email" required ref={emailRef} style={{backgroundColor:'rgba(180, 41, 249, 0.07)', height:'40px'}}></Form.Control>
                         </Form.Group>
 
                       
                         <br/>                      
 
-                        <Button disabled={loading} className="w-100" type="submit">Reset Password</Button>
+                        <Button disabled={loading} className="w-100" type="submit" style={{backgroundColor:'#B429F9', boxShadow:'0 8px 16px 0 #f1defa, 0 6px 20px 0 #e5bbfa', height:'50px'}}>Reset Now</Button>
                     </Form>
 
-                    <div className="w-100 text-center mt-3">
-                        <Link to="/login">Login</Link>
+                    <div style={{textAlign:'right'}}>
+                    <ForgotPassReact/>
+                        
                     </div>
-                </Card.Body>                
 
-            </Card>
+                    
 
-            <ForgotPassReact/>
+            </Segment>
             
         </div>
     )
