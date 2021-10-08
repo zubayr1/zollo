a2c7a28b0fee451ca1a0669fb0ee4cc35ca38e3a
Zubayr Khalid <54028598+ZubayrKal@users.noreply.github.com>
2021-10-09 00:05:21 +0200

    header aligninhg

--- src/components/HeaderFolder/Header.js
+++ src/components/HeaderFolder/Header.js
@@ -50,6 +50,14 @@ export default function Header() {
         </span>
       )
 
+    
+    const triggersmaller = (
+        <span>
+          <img src={usericon} style={{height:'30px', borderRadius:'50%'}} alt="usericon" />
+        </span>
+      )
+
+
 
     const options = [
     {
@@ -76,15 +84,15 @@ export default function Header() {
 
 
             <Grid>
-                <Grid.Row columns={2} verticalAlign="middle">
+                <Grid.Row columns={2} verticalAlign="middle" only='computer'>
 
-                    <GridColumn width={4}>
+                    <GridColumn tablet={3} computer={4}>
 
                         <Grid>
 
                             <GridRow columns={2} verticalAlign="middle">
 
-                                <GridColumn>
+                                <GridColumn  computer={8} widescreen={6} largeScreen={6}>
                                 <Link to="/">
                                     <img src={logo} style={{height:'40px'}} alt="logo"/>
                                 </Link>
@@ -92,12 +100,12 @@ export default function Header() {
                                 </GridColumn>
 
 
-                                <GridColumn>
+                                <GridColumn  computer={6} widescreen={8} largeScreen={8}>
 
                                     <form onSubmit={handleSubmit}>
                                         
                                         <input style={{backgroundColor:'#F3F3F3', height:'40px', width:'400px',borderWidth:'0px', paddingLeft:'20px', color:'#A5A5A5', outline:'none', borderRadius:'20px',}}
-                                        placeholder= "Search for location, event and more..."
+                                        placeholder= "Search for locations, events and places..."
                                         type="text"
                                         value={name}
                                         onChange={e => setName(e.target.value)}
@@ -118,35 +126,35 @@ export default function Header() {
 
 
 
-                    <GridColumn width={4} floated='right'>
+                    <GridColumn   computer={6}  widescreen={4} largeScreen={4} floated='right'>
 
                         <Grid>
 
                             <GridRow columns={4} verticalAlign="middle">
 
-                                <GridColumn width={8}>
-                                    <Button style={{backgroundColor:'#B429F9', boxShadow:'0 8px 16px 0 #f1defa, 0 6px 20px 0 #e5bbfa', height:'40px', width:'120px',color:'#FFFFFF', borderRadius:'20px'}}> <div style={{display:'inline',}}>+</div> Post</Button>
+                                <GridColumn  computer={6} widescreen={6} largeScreen={8}>
+                                    <Button style={{backgroundColor:'#B429F9', boxShadow:'0 8px 16px 0 #f1defa, 0 6px 20px 0 #e5bbfa', height:'40px', width:'120px',color:'#FFFFFF', borderRadius:'20px'}}>+ Post</Button>
 
 
                                 </GridColumn>
 
 
 
-                                <GridColumn width={2}>
-                                    <img src={notificationicon} style={{height:'30px'}} alt="notification"/>
+                                <GridColumn computer={2} widescreen={2} largeScreen={3} floated='right'>
+                                    <img src={notificationicon} style={{height:'25px'}} alt="notification"/>
 
                                 </GridColumn>
 
 
-                                <GridColumn width={2}>
-                                    <img src={chaticon} style={{height:'30px', display:'inline-block'}} alt="chat"/>
+                                <GridColumn  computer={2} widescreen={2} largeScreen={3} floated='right'>
+                                    <img src={chaticon} style={{height:'25px', display:'inline-block'}} alt="chat"/>
 
 
                                 </GridColumn>
 
 
 
-                                <GridColumn width={4} floated='right'>
+                                <GridColumn  computer={2} widescreen={4} largeScreen={2} floated='right'>
                                     
                                     <Dropdown trigger={trigger} options={options} pointing='top right' onChange={handledropdown} />
 
@@ -165,6 +173,201 @@ export default function Header() {
 
 
                 </Grid.Row>
+
+
+
+
+
+
+                <Grid.Row columns={2} verticalAlign="middle" only='tablet'>
+
+                    <GridColumn tablet={3} computer={4}>
+
+                        <Grid>
+
+                            <GridRow columns={2} verticalAlign="middle">
+
+                                <GridColumn tablet={12} >
+                                <Link to="/">
+                                    <img src={logo} style={{height:'30px'}} alt="logo"/>
+                                </Link>
+
+                                </GridColumn>
+
+
+                                <GridColumn  tablet={2} >
+
+                                    <form onSubmit={handleSubmit}>
+                                        
+                                        <input style={{backgroundColor:'#F3F3F3', height:'30px', width:'240px',borderWidth:'0px', paddingLeft:'20px', color:'#A5A5A5', outline:'none', borderRadius:'20px',}}
+                                        placeholder= "Search here..."
+                                        value={name}
+                                        onChange={e => setName(e.target.value)}
+                                        />
+                                    </form>
+
+                                </GridColumn>
+
+
+
+                            </GridRow>
+
+
+                        </Grid>
+
+                    </GridColumn>
+
+
+
+
+                    <GridColumn  tablet={6}  floated='right'>
+
+                        <Grid>
+
+                            <GridRow columns={4} verticalAlign="middle">
+
+                                <GridColumn tablet={4} >
+                                    <Button style={{backgroundColor:'#B429F9', boxShadow:'0 8px 16px 0 #f1defa, 0 6px 20px 0 #e5bbfa', height:'30px', width:'80px',color:'#FFFFFF', borderRadius:'20px'}}>Post</Button>
+
+
+                                </GridColumn>
+
+
+
+                                <GridColumn tablet={3}  floated='right'>
+                                    <img src={notificationicon} style={{height:'25px'}} alt="notification"/>
+
+                                </GridColumn>
+
+
+                                <GridColumn  tablet={3}  floated='right'>
+                                    <img src={chaticon} style={{height:'25px', display:'inline-block'}} alt="chat"/>
+
+
+                                </GridColumn>
+
+
+
+                                <GridColumn  tablet={2} floated='right'>
+                                    
+                                    <Dropdown trigger={triggersmaller} options={options} pointing='top right' onChange={handledropdown} />
+
+
+                                </GridColumn>
+
+
+
+                            </GridRow>
+                        </Grid>
+
+
+
+                    </GridColumn>
+
+
+
+                </Grid.Row>
+
+
+
+
+
+
+
+                <Grid.Row columns={2} verticalAlign="middle" only='mobile'>
+
+                    <GridColumn mobile={5}>
+
+                        <Grid>
+
+                            <GridRow columns={2} verticalAlign="middle">
+
+                                <GridColumn mobile={10} >
+                                <Link to="/">
+                                    <img src={logo} style={{height:'30px'}} alt="logo"/>
+                                </Link>
+
+                                </GridColumn>
+
+
+                                <GridColumn  mobile={1} floated='left'>
+
+                                    <form onSubmit={handleSubmit}>
+                                        
+                                        <input style={{backgroundColor:'#F3F3F3', height:'30px', width:'140px',borderWidth:'0px', paddingLeft:'20px', color:'#A5A5A5', outline:'none', borderRadius:'20px',}}
+                                        placeholder= "Search ..."
+                                        value={name}
+                                        onChange={e => setName(e.target.value)}
+                                        />
+                                    </form>
+
+                                </GridColumn>
+
+
+
+                            </GridRow>
+
+
+                        </Grid>
+
+                    </GridColumn>
+
+
+
+
+                    <GridColumn  mobile={7}  floated='right'>
+
+                        <Grid>
+
+                            <GridRow columns={4} verticalAlign="middle">
+
+                                <GridColumn mobile={4} >
+                                    <Button style={{backgroundColor:'#B429F9', boxShadow:'0 8px 16px 0 #f1defa, 0 6px 20px 0 #e5bbfa', height:'30px', width:'50px',color:'#FFFFFF', borderRadius:'20px'}}>+</Button>
+
+
+                                </GridColumn>
+
+
+
+                                <GridColumn mobile={3}  floated='right'>
+                                    <img src={notificationicon} style={{height:'25px'}} alt="notification"/>
+
+                                </GridColumn>
+
+
+                                <GridColumn  mobile={3}  floated='right'>
+                                    <img src={chaticon} style={{height:'25px', display:'inline-block'}} alt="chat"/>
+
+
+                                </GridColumn>
+
+
+
+                                <GridColumn  mobile={3}  floated='right'>
+                                    
+                                    <Dropdown trigger={triggersmaller} options={options} pointing='top right' onChange={handledropdown} />
+
+
+                                </GridColumn>
+
+
+
+                            </GridRow>
+                        </Grid>
+
+
+
+                    </GridColumn>
+
+
+
+                    </Grid.Row>
+
+
+
+
+
+
 
 
             
